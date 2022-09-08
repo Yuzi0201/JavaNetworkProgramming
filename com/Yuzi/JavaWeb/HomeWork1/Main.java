b0604a9155ca1d2876aaeb10eccadf7e4b4c389a
Yuzi0201 <2205816944@qq.com>
2022-09-08 21:21:59 +0800

    WebHomeWork1

---
+++ com/Yuzi/JavaWeb/HomeWork1/Main.java
@@ -0,0 +1,9 @@
+package com.Yuzi.JavaWeb.HomeWork1;
+
+public class Main {
+  public static void main(String[] args){
+    Window window = new Window();
+    window.setBounds(400, 200, 400, 300);
+    window.setTitle("十进制转换");
+  }
+}
