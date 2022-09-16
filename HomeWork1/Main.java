4478c6175b6f59d3020fc70ad02287560adefb5b
Yuzi0201 <2205816944@qq.com>
2022-09-16 11:24:32 +0800

    delete files

--- HomeWork1/Main.java
+++
@@ -1,9 +0,0 @@
-package com.Yuzi.JavaWeb.HomeWork1;
-
-public class Main {
-  public static void main(String[] args){
-    Window window = new Window();
-    window.setBounds(400, 200, 400, 300);
-    window.setTitle("十进制转换");
-  }
-}
