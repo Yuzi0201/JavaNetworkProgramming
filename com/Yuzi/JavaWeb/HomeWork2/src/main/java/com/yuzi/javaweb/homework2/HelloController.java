49aed21f411bb6a179533b7ea803b82bf4634a4a
Yuzi0201 <2205816944@qq.com>
2022-09-18 09:26:43 +0800

    code format

--- com/Yuzi/JavaWeb/HomeWork2/src/main/java/com/yuzi/javaweb/homework2/HelloController.java
+++ com/Yuzi/JavaWeb/HomeWork2/src/main/java/com/yuzi/javaweb/homework2/HelloController.java
@@ -199,14 +199,12 @@ public class HelloController {
       try {
         inputStream.close();
       } catch (IOException e) {
-        e.printStackTrace(); // To change body of catch statement use
-        // File | Settings | File Templates.
+        e.printStackTrace();
       }
       try {
         outputStream.close();
       } catch (IOException e) {
-        e.printStackTrace(); // To change body of catch statement use
-        // File | Settings | File Templates.
+        e.printStackTrace();
       }
     }
     return false;
